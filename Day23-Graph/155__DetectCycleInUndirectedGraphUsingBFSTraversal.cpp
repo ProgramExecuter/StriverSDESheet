8d07744662f6f602a6ee8508ddc730b67022c88d
Amit Singh Rana <ranaa73000@gmail.com>
2022-04-05 17:38:56 +0530

    'Detect Cycle In Undirected Graph using BFS Traversal'  --MEDIUM--  Solved

--- Day23-Graph/155__DetectCycleInUndirectedGraphUsingBFSTraversal.cpp
+++ Day23-Graph/155__DetectCycleInUndirectedGraphUsingBFSTraversal.cpp
@@ -1,5 +1,5 @@
 // Time Complexity - O(N)
-// Space Complexity - O(N)
+// Space Complexity - O(N + N)  ==>  queue  +  'vis' array
 
 //**
 //  SKELETON OF "ADJ"
@@ -13,15 +13,15 @@
 //**
 
 class Solution {
-public:
-    bool hasCycle(int st, vector<bool>& vis, vector<vector<int>> &adj) {
+  public:
+    bool hasCycle(int st, vector<bool>& vis, vector<int> adj[]) {
         queue<pair<int, int>> q;
         q.push({st, -1});
         vis[st] = true;
         
         while(!q.empty()) {
             int curr = q.front().first;         // Get front of QUEUE
-            int par  = q.back().second;         // Get 'parent'
+            int par  = q.front().second;         // Get 'parent'
             q.pop();
             
             // Neighbours of 'curr'
@@ -38,18 +38,16 @@ public:
         
         return false;
     }
-    bool canFinish(int v, vector<vector<int>>& adj) {
+    bool isCycle(int v, vector<int> adj[]) {
         vector<bool> vis(v, 0);
-        
-        vector<vector<int>> adj = convert(adj, v);  // Convert to adjancency mat, for graph
-        
+
         for(int i=0; i<v; ++i) {
             if(vis[i])  continue;                   // Already visited, SKIP
             
             if(hasCycle(i, vis, adj))               // Has a cycle
-                return false;
+                return true;
         }
         
-        return true;
+        return false;
     }
 };
